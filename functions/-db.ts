f2a6417916b81dd4321f8b2f7ea4b48246260b31
alfons149-cmyk <alfons149@gmail.com>
2026-02-12 00:06:57 +0100

    Create -db.ts

---
+++ functions/-db.ts
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/d1";
+import type { DrizzleD1Database } from "drizzle-orm/d1";
+
+export type Env = { DB: D1Database };
+
+export function getDb(env: Env): DrizzleD1Database {
+  return drizzle(env.DB);
+}
