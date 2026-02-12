228f332eeec66784657083c22a827b5444a68b29
alfons149-cmyk <alfons149@gmail.com>
2026-02-12 13:06:12 +0100

    Update seed.ts

--- functions/api/seed.ts
+++ functions/api/seed.ts
@@ -1,5 +1,5 @@
 import type { PagesFunction } from "@cloudflare/workers-types";
-import { getDb, type Env } from "./_db";
+import { getDb, type Env } from "./_db.ts";
 import { followups } from "./db/schema";
 
 export const onRequestPost: PagesFunction<Env> = async ({ env }) => {
