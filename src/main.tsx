90f96ce4e0d5eb6040558245c6aa263ae2d748e4
alfons149-cmyk <alfons149@gmail.com>
2026-02-19 17:40:27 +0100

    Store API key from URL in localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./App.css";
+import App from "./App.tsx";
+
+// --- API key via URL ?key=... opslaan ---
 const urlParams = new URLSearchParams(window.location.search);
 const keyFromUrl = urlParams.get("key");
 
 if (keyFromUrl) {
   localStorage.setItem("VD_API_KEY", keyFromUrl);
   console.log("API key opgeslagen");
+  // optioneel: haal key uit de URL (netter)
+  urlParams.delete("key");
+  const newUrl =
+    window.location.pathname +
+    (urlParams.toString() ? `?${urlParams.toString()}` : "") +
+    window.location.hash;
+  window.history.replaceState({}, "", newUrl);
 }
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import "./App.css";
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
