c78095d4801c62b37c9d54eea4feeece0a35b995
alfons149-cmyk <alfons149@gmail.com>
2026-02-20 15:14:10 +0100

    Add useRef import to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useRef } from "react";
 import "./App.css";
 
 type Status = "open" | "sent" | "waiting" | "followup" | "done";
